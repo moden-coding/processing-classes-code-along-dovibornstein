46266e1e55b2b9d6a38df959cb959baff8c7962e
dovibornstein <dovi.bornstein@gmail.com>
2026-01-08 15:59:17 -0500

    in progress

--- src/App.java
+++ src/App.java
@@ -1,20 +1,117 @@
 import processing.core.*;
 
-public class App extends PApplet{
-    public static void main(String[] args)  {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class App extends PApplet {
+    ArrayList<Bubble> bubbles;
+    double timer;
+    int scene;
+    double highScore;
+    double gameStart;
+
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-    public void setup(){
-        
+    public void setup() {
+        bubbles = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            bubbleMaker();
+        }
+
+        scene = 0;
+        gameStart = millis();
     }
 
-    public void settings(){
-        
+    public void settings() {
+        size(1200, 800);
     }
 
-    public void draw(){
-       
+    public void draw() {
+        background(0);
 
+        if (scene == 0) {
+            for (Bubble b : bubbles) {
+                b.display();
+                b.update();
+            }
+
+            fill(255);
+            textSize(50);
+            timer = millis() - gameStart;
+            timer = ((int) timer / 100) / 10.0;
+            text("" + timer, width - 100, 50);
+            if (bubbles.size() == 0) {
+                scene = 1;
+                readHighScore();
+
+                if (highScore == 0 || highScore > timer) {
+                    highScore = timer;
+                    saveHighScore();
+                }
+
+            }
+        } else {
+            text("Score " + timer, 400, 400);
+            text("High score: " + highScore, 400, 500);
+        }
+    }
+
+    public void saveHighScore() {
+        try (PrintWriter writer = new PrintWriter("highscore.txt")) {
+            writer.println(highScore); // Writes the integer to the file
+            writer.close(); // Closes the writer and saves the file
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void readHighScore() {
+        try (Scanner scanner = new Scanner(Paths.get("highscore.txt"))) {
+
+            // we read the file until all lines have been read
+            while (scanner.hasNextLine()) {
+                // we read one line
+                String row = scanner.nextLine();
+                // we print the line that we read
+                highScore = Double.valueOf(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+    public void bubbleMaker() {
+        int x = (int) random(800) + 125;
+        int y = (int) random(800);
+
+        Bubble bubble = new Bubble(x, y, this);
+        bubbles.add(bubble);
+    }
+
+    public void keyPressed() {
+        if (key == ' ') {
+            if (scene == 0) {
+                bubbles.clear();
+            } else {
+                setup();
+            }
+        }
+    }
+
+    public void mousePressed() {
+        for (int i = 0; i < bubbles.size(); i++) {
+            Bubble b = bubbles.get(i);
+            if (b.checkTouch(mouseX, mouseY) == false) {
+                bubbles.remove(b);
+            }
+        }
     }
 }
---
+++ src/Bubble.java
@@ -0,0 +1,63 @@
+import processing.core.PApplet;
+
+public class Bubble {
+    private int x;
+    private int y;
+    private int size;
+    private PApplet canvas;
+    private int speed;
+    private int color;
+    private int health;
+
+    public Bubble(int xPos, int yPos, PApplet c) {
+        x = xPos;
+        y = yPos;
+        size = 250;
+        canvas = c;
+        speed = 5;
+        color = canvas.color(0, 255, 0);
+        health = 3;
+    }
+
+    public void display() {
+        canvas.fill(color);
+        canvas.circle(x, y, size);
+        if (health == 2) {
+            color = canvas.color(255, 255, 0);
+
+        } else if (health == 1) {
+            color = canvas.color(255, 0, 0);
+        }
+    }
+
+    public void update() {
+        x += speed;
+        if (x + size / 2 > canvas.width || x - size / 2 < 0) {
+            speed = -speed;
+        }
+    }
+
+    public int randomColor() {
+        return color = canvas.color(canvas.random(255), canvas.random(255), canvas.random(255));
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean checkTouch(int mouseX, int mouseY) {
+        float distanceFromCenter = canvas.dist(x, y, mouseX, mouseY);
+
+        if (distanceFromCenter < size / 2) {
+            health--;
+        }
+
+        if (health > 0) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+}
